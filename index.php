6d381baaf0a63ac819480f2b7c7b5831932da6c3
Yann Lederrey <yann.lederrey@heig-vd.ch>
2021-03-03 10:52:19 +0100

    adding some details scenarios

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>PHP File Upload example</title>
+</head>
+<body>
+
+<form action="fileupload.php" enctype="multipart/form-data" method="post">
+Select image :
+<input type="file" name="file"><br/>
+<input type="submit" value="Upload" name="Submit1"> <br/>
+
+
+</form>
+
+</body>
+</html>
